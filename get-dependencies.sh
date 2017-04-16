7868fbc991787f86851494099d1e40a35fd55033
juju4 <juju4@users.noreply.github.com>
2017-04-16 09:47:30 -0400

    fix syntax and cp command

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,8 +11,8 @@ fi
 
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $rolesdir/kbrebabnov.osquery ] && ln -s ansible-myrole $rolesdir/kbrebabnov.osquery
-[ ! -e $rolesdir/kbrebabnov.osquery ] && cp -R $rolesdir/ansible-myrole $rolesdir/kbrebabnov.osquery
+#[ ! -e $rolesdir/kbrebabnov.osquery ] && ln -s ansible-osquery $rolesdir/kbrebabnov.osquery
+[ ! -e $rolesdir/kbrebabnov.osquery ] && cp -R $rolesdir/ansible-osquery $rolesdir/kbrebabnov.osquery
 
 ## don't stop build on this script return code
 true
