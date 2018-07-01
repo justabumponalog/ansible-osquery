8850b3d0c8036c7837a8de6c0fc0455f565c6ed6
juju4 <juju4@users.noreply.github.com>
2018-06-30 21:37:19 -0400

    add test suite for osquery+kolide fleetmanager

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,7 @@ rolesdir=$(dirname $0)/..
 fi
 
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.kolide ] && git clone https://github.com/juju4/ansible-kolide $rolesdir/juju4.kolide
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/kbrebanov.osquery ] && ln -s ansible-osquery $rolesdir/kbrebanov.osquery
 [ ! -e $rolesdir/kbrebanov.osquery ] && cp -R $rolesdir/ansible-osquery $rolesdir/kbrebanov.osquery
