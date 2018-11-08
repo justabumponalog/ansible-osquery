5ef3d4ccf60703b04489ff0c747aba1cd1a85369
juju4 <juju4@users.noreply.github.com>
2018-11-07 20:01:57 -0500

    fix typo

--- test/integration/default-profiling/serverspec/osquery_load_spec.rb
+++ test/integration/default-profiling/serverspec/osquery_load_spec.rb
@@ -21,7 +21,7 @@ describe file('/var/log/osquery/osqueryd.results.log') do
   it { should_not contain '"target_path":"\/var\/' }
   it { should_not contain '"target_path":"\/tmp\/too_many_events\/testing-aa' }
   it { should_not contain '"target_path":"\/tmp\/too_many_events\/testing-hh' }
-  it { should_not contain '"target_path":"\/tmp\/too_many_events2\/test/testing-aa' }
+  it { should_not contain '"target_path":"\/tmp\/too_many_events2\/test\/testing-aa' }
   it { should_not contain '"target_path":"\/tmp\/excluded1-abcdef' }
   it { should_not contain '"target_path":"\/tmp\/abcdef-excluded2' }
   it { should_not contain '"target_path":"\/tmp\/excluded3-abcdef-testing' }
