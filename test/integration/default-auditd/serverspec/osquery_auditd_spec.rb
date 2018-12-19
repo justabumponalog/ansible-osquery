b80302f06ae448a01fb4d3ab2de8dc21ea5c9378
juju4 <juju4@users.noreply.github.com>
2018-12-19 01:36:25 -0500

    add serverspec tests for auditd option

---
+++ test/integration/default-auditd/serverspec/osquery_auditd_spec.rb
@@ -0,0 +1,30 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe service('auditd') do
+  it { should_not be_enabled }
+  it { should_not be_running }
+end
+
+describe command('auditctl -s') do
+  its(:stdout) { should_not match /enable 0/ }
+  its(:stderr) { should match /^$/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('auditctl -l') do
+  its(:stdout) { should match /-a always,exit -S read/ }
+  its(:stdout) { should match /-a always,exit -S write/ }
+  its(:stdout) { should match /-a always,exit -S clone/ }
+  its(:stdout) { should match /-a always,exit -S bind/ }
+  its(:stderr) { should match /^$/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/var/log/osquery/osqueryd.results.log') do
+  its(:content) { should match /{"name":"process_events","hostIdentifier":/ }
+  its(:content) { should match /{"name":"socket_events","hostIdentifier":/ }
+  its(:content) { should match /{"name":"fim","hostIdentifier":/ }
+end
