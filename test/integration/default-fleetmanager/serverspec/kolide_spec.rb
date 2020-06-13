10eff8b9b7500b56c7a4bdeccfca2c98bf7ca7ab
juju4 <juju4@users.noreply.github.com>
2020-06-12 21:02:56 -0400

    test/default-fleetmanager: review serverspec tests

--- test/integration/default-fleetmanager/serverspec/kolide_spec.rb
+++ test/integration/default-fleetmanager/serverspec/kolide_spec.rb
@@ -34,6 +34,12 @@ describe file('/var/log/messages'), :if => os[:family] == 'redhat' do
   its(:content) { should match /kolide: Using config file:/ }
   its(:content) { should match /kolide: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
   its(:content) { should_not match /kolide: .*permission denied.*/ }
+  its(:content) { should_not match /kolide: .*remote error: tls: internal error.*/ }
+end
+
+describe file('/etc/osquery/osquery_enroll_secret') do
+  it { should be_file }
+  its(:size) { should > 0 }
 end
 
 # Note: probably requires fleet initial configuration...
@@ -52,4 +58,26 @@ describe command('curl -vk -X GET https://localhost:8080/api/v1/hosts') do
   its(:stdout) { should match /Unknown Error/ }
 #  its(:stderr) { should match /HTTP\/1.1 500 / }
 #  its(:exit_status) { should_not eq 0 }
+end
+
+describe command('openssl s_client -connect localhost:8080 < /dev/null 2>/dev/null | openssl x509 -text -in /dev/stdin') do
+  its(:stdout) { should match /Issuer: O = Fleet by Ansible, CN = kolide/ }
+  its(:stdout) { should match /sha256/ }
+  its(:stdout) { should match /RSA Public-Key: \(4096 bit\)/ }
+end
+
+describe command('fleetctl get hosts') do
+  its(:stdout) { should match /ubuntu.*online/ }
+  its(:stdout) { should_not match /no hosts found/ }
+  its(:stderr) { should match /^$/ }
+  its(:exit_status) { should_not eq 0 }
+end
+
+describe command('fleetctl query --query "select * from users;" --hosts "All Hosts"') do
+  its(:stdout) { should match /root/ }
+  its(:stdout) { should match /daemon/ }
+  its(:stdout) { should match /nobody/ }
+  its(:stdout) { should_not match /0\/0 online/ }
+  its(:stderr) { should match /^$/ }
+  its(:exit_status) { should_not eq 0 }
 end
